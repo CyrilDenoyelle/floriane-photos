0a6cc06fc42e35fc39bbd7a85fbc4d52ab6c0241
CyrilD <cyril.denoyelle@morning.com>
2018-09-15 14:08:21 +0200

    add walker helper and fs

---
+++ helper/walker.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+module.exports = (dir) => {
+    const done = (error) => {
+        if (error) {
+            throw error;
+        } else {
+            // console.log('-------------------------------------------------------------');
+            // console.log('finished.', allFilesPaths);
+            // console.log('-------------------------------------------------------------');
+        };
+    };
+
+    allFilesPaths = [];
+    fs.readdir(dir, (error, list) => {
+        if (error) {
+            return done(error);
+        }
+
+        let i = 0;
+
+        (function next() {
+            let file = list[i++];
+
+            if (!file) {
+                return done(null);
+            }
+
+            file = dir + '/' + file;
+
+            fs.stat(file, function (error, stat) {
+
+                if (stat && stat.isDirectory()) {
+                    walk(file, function (error) {
+                        next();
+                    });
+                } else {
+                    // do stuff to file here
+                    // console.log(stat);
+                    allFilesPaths.push(file);
+                    next();
+                }
+            });
+        })();
+    });
+    return allFilesPaths;
+};
+
+// optional command line params
+//      source for walk path
+process.argv.forEach((val, index, array) => {
+    if (val.indexOf('source') !== -1) {
+        walkPath = val.split('=')[1];
+    }
+});
--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 8000
 
+const walker = require('./helper/walker.js');
+const imgsUrls = walker('imgs');
+
+// console.log(imgsUrls);
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index', { imgsUrls: imgsUrls }))
+  // .get('/imgs', (req, res) => res.render('pages/index', { imgsUrls }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
